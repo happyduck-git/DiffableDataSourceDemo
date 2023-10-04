75076132e83f6fd4288c3514c463a27c4f94d23f
GG <rkrudtls@gmail.com>
2023-10-04 23:16:56 +0900

    UITableView with plan UITableViewDelegate and UITableViewDataSource

--- DiffableDataSourceDemo/ViewController.swift
+++ DiffableDataSourceDemo/ViewController.swift
@@ -9,11 +9,64 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var addButton: UIButton!
+    private var number: Int = 4
+    
+    private var model: [String] = []
+    
+    private let table: UITableView = {
+  
+        let table = UITableView(frame: .zero, style: .grouped)
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        table.translatesAutoresizingMaskIntoConstraints = false
+        
+        return table
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.table.delegate = self
+        self.table.dataSource = self
+        self.view.addSubview(self.table)
+        self.table.frame = view.bounds
+        self.makeModel {
+            self.table.reloadData()
+        }
+
+        
     }
 
-
+    @IBAction func addData(_ sender: Any) {
+        self.model.append("Data no. \(self.number)")
+        self.number += 1
+        self.table.reloadData()
+    }
+    
 }
 
+extension ViewController {
+    private func makeModel(completion: @escaping () -> Void) {
+        for i in 0...3 {
+            model.append("Data no. \(i)")
+        }
+        completion()
+    }
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.model.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell") else {
+            fatalError()
+        }
+        var config = cell.defaultContentConfiguration()
+        config.text = self.model[indexPath.row]
+        cell.contentConfiguration = config
+        
+        return cell
+    }
+}
